8bd21e597bb107677fc97b6a0b31b749cb696d07
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-02-17 09:16:46 -0500

    add line of tests

--- dmoj/uhscsonetwo/main.cc
+++ dmoj/uhscsonetwo/main.cc
@@ -1,74 +1,44 @@
-// #include "utils.h"
-#include <algorithm>
 #include <iostream>
-#include <iterator>
-#include <set>
 #include <utility>
 #include <vector>
 using namespace std;
 
 int main() {
-  size_t number_of_marks;
-  cin >> number_of_marks;
+  int num_of_tests;
+  cin >> num_of_tests;
 
-  set<size_t> mark_indices_90;
-  for (size_t i = 0; i < number_of_marks; ++i) {
-    size_t mark;
+  vector<int> marks_above_90(num_of_tests);
+  for (int i = 0; i < num_of_tests; ++i) {
+    int mark;
     cin >> mark;
     if (mark >= 90) {
-      mark_indices_90.emplace(i);
+      marks_above_90[i] = 1;
     }
   }
 
-  if (mark_indices_90.size() < 1) {
-    cout << 0;
-    return 0;
-  }
+  int num_of_ranges;
+  cin >> num_of_ranges;
 
-  size_t number_of_ranges;
-  cin >> number_of_ranges;
-
-  // 0 based
-  vector<pair<size_t, size_t>> ranges(number_of_ranges);
-  for (size_t i = 0; i < number_of_ranges; ++i) {
-    size_t lrange, rrange;
+  vector<pair<int, int>> ranges(num_of_ranges);
+  for (int i = 0; i < num_of_ranges; ++i) {
+    int lrange, rrange;
     cin >> lrange >> rrange;
     ranges[i] = make_pair(lrange - 1, rrange - 1);
   }
 
-  size_t max_90_or_more{0};
-
-  for (auto [lrange, rrange] : ranges) {
-    auto actual_lrange =
-        find_if(mark_indices_90.begin(), mark_indices_90.end(),
-                [lrange](const size_t index) { return index >= lrange; });
-
-    auto actual_rrange =
-        find_if(mark_indices_90.rbegin(), mark_indices_90.rend(),
-                [rrange](const size_t index) { return index <= rrange; });
-
-    // clog << "range: " << lrange << "-" << rrange << "\n";
-    // clog << "actual_lrange: " << *actual_lrange << "\n";
-    // clog << "actual_rrange: " << *actual_rrange << "\n";
-
-    const auto distance = [&actual_lrange, &actual_rrange]() -> size_t {
-      auto distance = 0;
-
-      while (*actual_lrange <= *actual_rrange) {
-        ++distance;
-        ++actual_lrange;
-      }
-
-      return distance;
-    }();
-
-    if (distance > max_90_or_more) {
-      max_90_or_more = distance;
-    }
+  vector<int> marks_psa(num_of_tests + 1);
+  for (int i = 0; i < num_of_tests; ++i) {
+    marks_psa[i + 1] = marks_psa[i] + marks_above_90[i];
   }
 
-  // cout << "result:\n";
-  cout << max_90_or_more;
+  int max_tests = 0;
+
+  for (const auto &[lrange, rrange] : ranges) {
+    int tests = marks_psa[rrange + 1] - marks_psa[lrange];
+    max_tests = max(max_tests, tests);
+  }
+
+  cout << max_tests;
 
   return 0;
 }
